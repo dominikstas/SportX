e92d8c6e41bcf54d6f2eab78566e1bd6ed765e3b
Dominik Stasiak <d.stasiak436@gmail.com>
2023-09-08 09:36:08 +0200

    utworzono formularz rejestracji

--- sportx/login/views.py
+++ sportx/login/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def login(request):
-    return render(request, "login/login.html")
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    context = {'form':form}
+    return render(request, "login/login.html", context)
+    
