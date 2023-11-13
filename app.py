f87783884d16639b9557809952380081f6314a59
Dominik Stasiak <d.stasiak436@gmail.com>
2023-11-13 18:26:05 +0100

    add dashboard and logout

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import secrets
@@ -27,22 +27,34 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
         if user and bcrypt.check_password_hash(user.password, password):
+            session['user_id'] = user.id  # Zapisuje ID użytkownika w sesji
             flash('Login successful!', 'success')
-            return render_template('home.html')
+            return redirect(url_for('dashboard'))
         else:
             flash('Login unsuccessful. Please check your username and password.', 'danger')
     return render_template('login.html')
 
+@app.route("/dashboard")
+def dashboard():
+    if 'user_id' in session:
+        user = User.query.get(session['user_id'])
+        return render_template('dashboard.html', username=user.username)
+    else:
+        flash('You need to log in first.', 'warning')
+        return redirect(url_for('login'))
 
-
-
+@app.route("/logout")
+def logout():
+    session.pop('user_id', None)
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
